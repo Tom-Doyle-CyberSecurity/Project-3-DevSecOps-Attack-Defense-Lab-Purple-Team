426294f35d9a59ab87015d96d4ed9325d6b66fe9
Tom-Doyle-CyberSecurity <133623349+Tom-Doyle-CyberSecurity@users.noreply.github.com>
2025-07-02 06:15:54 +0000

    .gitignore and variables.tf with documentation

--- Terraform-IaC/variables.tf
+++ Terraform-IaC/variables.tf
@@ -2,6 +2,12 @@
 # Input-Variables for DevSecOps Infrastructure
 ##############################################
 
+# AMI ID
+variable "ami_id" {
+    description = "AMI ID of the EC2 instance"
+    type = string
+}
+
 # Name of the key pair for SSH access (must exist in AWS / preferred cloud provider)
 variable "key_name" {
     description = "Name of existing AWS EC2 key pair"
@@ -19,4 +25,16 @@ variable "instance_type" {
     description = "AWS EC2 instance_type"
     type = string
     default = "t3.micro"
+}
+
+# VPC ID
+variable "vpc_id"{
+    description = "VPC ID of the EC2 instance"
+    type = string
+}
+
+# Subnet ID
+variable "subnet_id" {
+    description = "Subnet ID of the EC2 instance"
+    type = string
 }
