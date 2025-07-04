dfef044c26818e2159cc11a421c3a729e07388e4
Tom-Doyle-CyberSecurity <133623349+Tom-Doyle-CyberSecurity@users.noreply.github.com>
2025-07-04 13:26:29 +0000

    Windows syntax fixes and modularisation

--- Instances/Windows_2022_Instance/Windows2022_Instance.tf
+++ Instances/Windows_2022_Instance/Windows2022_Instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "windows_server" {
     ami = var.windows_ami_id
-    instance_type = var.instance_type
+    instance_type = var.windows_instance_type
     key_name = var.key_name
     subnet_id = var.subnet_id
     vpc_security_group_ids = [var.security_group_id]
--- Instances/Windows_2022_Instance/Windows_variables.tf
+++ Instances/Windows_2022_Instance/Windows_variables.tf
@@ -1,24 +1,32 @@
+########################
 # Windows Server 2022 VM
+########################
+
+# Windows AMI ID
 variable "windows_ami_id" {
     description = "AMI ID for Windows Server 2022"
     type = string
 }
 
-variable "instance_type" {
+# Windows Instance Type
+variable "windows_instance_type" {
     description = "Instance type for Windows Server 2022"
     type = string
 }
 
+# Windows Key Name
 variable "key_name" {
     description = "Key name for Windows Server 2022"
     type = string
 }
 
+# Windows Subnet ID
 variable "subnet_id" {
     description = "Subnet ID for Windows Server 2022"
     type = string
 }
 
+# Windows Security Group
 variable "security_group_id" {
     description = "Security group ID for Windows Sever 2022"
     type = string
