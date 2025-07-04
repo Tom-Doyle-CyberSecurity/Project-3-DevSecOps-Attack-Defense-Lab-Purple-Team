86299c2009aeefb28770e8e1245a177c1a5e68d4
Tom-Doyle-CyberSecurity <133623349+Tom-Doyle-CyberSecurity@users.noreply.github.com>
2025-07-04 13:23:06 +0000

    Syntax fixes & further modularisation

--- Instances/Amazon_Linux2_Instance/Amazon_host.tf
+++ Instances/Amazon_Linux2_Instance/Amazon_host.tf
@@ -1,9 +1,9 @@
 # Launch an EC2 instance using Amazon Linux 2 and attach the security group
 resource "aws_instance" "docker_host" {
-    ami = var.ami_id
-    instance_type = var.instance_type
+    ami = var.A2_ami_id
+    instance_type = var.A2_instance_type
     key_name = var.key_name
-    vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+    vpc_security_group_ids = [var.security_group_id]
 
     # This script installs Docker and runs docker-compose on the EC2 instance
     user_data = file("${path.module}/../setup.sh")
--- Instances/Amazon_Linux2_Instance/Amazon_host_variables.tf
+++ Instances/Amazon_Linux2_Instance/Amazon_host_variables.tf
@@ -1,4 +1,33 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "security_group_id" {}
+##########################
+# Amazon Linux 2 Variables
+##########################
+
+# AMI ID
+variable "A2_ami_id" {
+    description = "AMI ID of Amazon Linux 2"
+    type = string
+}
+
+# Instance type
+variable "A2_instance_type" {
+    description = "Instance Type"
+    type = string
+}
+
+# Key name
+variable "key_name" {
+    description = "Key name"
+    type = string
+}
+
+# Subnet ID
+variable "subnet_id" {
+    description = "Subnet ID"
+    type = string
+}
+
+# Security Group ID
+variable "security_group_id" {
+    description = "Security Group ID"
+    type = string
+}
