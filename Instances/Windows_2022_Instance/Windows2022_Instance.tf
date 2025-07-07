0035bfa63570ffeb6089801aae98b0312cafa282
Tom-Doyle-CyberSecurity <133623349+Tom-Doyle-CyberSecurity@users.noreply.github.com>
2025-07-07 14:03:05 +0000

    Windows 2022 Server ami id

--- Instances/Windows_2022_Instance/Windows2022_Instance.tf
+++ Instances/Windows_2022_Instance/Windows2022_Instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "windows_server" {
-    ami = data.aws_ami.windows_server_2022.id
+    ami = var.windows_ami_id
     instance_type = var.windows_instance_type
     key_name = var.key_name
     subnet_id = var.subnet_id
